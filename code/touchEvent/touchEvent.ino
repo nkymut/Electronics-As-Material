2499f256535f4af921d3590f1457a4c39f85c4fd
Yuta Nakayama <nkymut@gmail.com>
2025-10-16 15:34:41 +0800

    Add touchEvent sample

---
+++ code/touchEvent/touchEvent.ino
@@ -0,0 +1,25 @@
+#include <CapacitiveSensor.h> // add the CapacitiveSensor library
+
+long capVal = 0;
+long prevCapVal = 0;
+long thresh = 1000;
+
+CapacitiveSensor cs_4_2 = CapacitiveSensor(4, 2); // 1MOhm resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+
+void setup() {
+  cs_4_2.set_CS_Timeout_Millis(50); // set a limit to how long the capacitive sensor takes for each reading
+  Serial.begin(9600); // begin serial so we can communicate with the Arduino over USB
+}
+
+void loop() {
+  capVal =  cs_4_2.capacitiveSensor(30);
+   if(capVal > thresh & prevCapVal < thresh ){
+     Serial.println("touched");
+   } else if(capVal <= thresh & prevCapVal >= thresh){
+     Serial.println("released");
+   }
+ 
+  prevCapVal = capVal;
+
+  delay(50); // arbitrary delay to limit data to serial port
+}
